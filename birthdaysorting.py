a372dd0109c29b60add95ff51b3a9cfc9deae682
anafberhanu877-lang <anafberhanu877@gmail.com>
2026-09-01 11:59:39 -0400

    Add files via upload

---
+++ birthdaysorting.py
@@ -0,0 +1,11 @@
+jagdish = "January 15th"
+anaf = "March 22nd"
+jeff = "July 8th"
+timmy = "September 12th"
+jhon = "November 30th"
+
+print("Jagdish's birthday is on", jagdish)
+print("Anaf's birthday is on", anaf)
+print("Jeff's birthday is on", jeff)
+print("Timmy's birthday is on", timmy)
+print("Jhon's birthday is on", jhon)
